1dc7be876c0bf4e9479c755cdef0cc43d9fc76d5
Nathan Hart <Nathan Hart@NATHAN-COBALT>
2023-03-05 12:59:17 +0000

    Add project files.

---
+++ PiVisualisations/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PiVisualisations
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Draw()
+        {
+            Random myRNG = new Random();
+
+            SolidColorBrush newColour = new SolidColorBrush(Color.FromRgb((byte)myRNG.Next(1, 255), (byte)myRNG.Next(1, 255), (byte)myRNG.Next(1, 255)));
+            Rectangle myRect = new Rectangle
+            {
+                Width = 100,
+                Height = 100,
+                Fill = newColour,
+                StrokeThickness = 3,
+                Stroke = Brushes.Black
+            };
+
+            Canvas.SetLeft(myRect, 0);
+            Canvas.SetTop(myRect, 0);
+            Canvas.Children.Add(myRect);
+        }
+    }
+}
---
+++ PiVisualisations/View/UserControls/TopMenu.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PiVisualisations.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for TopMenu.xaml
+    /// </summary>
+    public partial class TopMenu : UserControl
+    {
+        public TopMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void TopMenu_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void TopMenu_Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Refresh clicked");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow window = Window.GetWindow(this) as MainWindow;
+            window.Draw();
+        }
+    }
+}
